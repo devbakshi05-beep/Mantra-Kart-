971a2aeef0c4ddc232d07018254da4b35e6b5312
bakshi05 <devbakshi0524@gmail.com>
2026-07-14 23:08:23 +0530

    Remove email confirmation

--- Ecom_Project_2026/Areas/Customer/Controllers/CartController.cs
+++ Ecom_Project_2026/Areas/Customer/Controllers/CartController.cs
@@ -76,25 +76,25 @@ namespace Ecom_Project_2026.Areas.Customer.Controllers
             var claims = claimsIdendity.FindFirst(ClaimTypes.NameIdentifier);
 
             var user = _unitOfWork.ApplicationUser.FirstOrDefault(au => au.Id == claims.Value);
-            if (user == null)
-            {
-                ModelState.AddModelError(string.Empty, "Email Empty !!!");
-            }
-            else
-            {
-                //Email
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code },
-                    protocol: Request.Scheme);
+            //if (user == null)
+            //{
+            //    ModelState.AddModelError(string.Empty, "Email Empty !!!");
+            //}
+            //else
+            //{
+            //    //Email
+            //    var userId = await _userManager.GetUserIdAsync(user);
+            //    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            //    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            //    var callbackUrl = Url.Page(
+            //        "/Account/ConfirmEmail",
+            //        pageHandler: null,
+            //        values: new { area = "Identity", userId = userId, code = code },
+            //        protocol: Request.Scheme);
 
-                await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-            }
+            //    await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
+            //        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+            //}
             return RedirectToAction(nameof(Index));
         }
         public IActionResult plus(int id)
