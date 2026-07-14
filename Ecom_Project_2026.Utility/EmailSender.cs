92d3c4a87a62ed761e9c34cd8a23147b9ab74337
bakshi05 <devbakshi0524@gmail.com>
2026-07-14 16:20:13 +0530

    BrevoEmail sender used

--- Ecom_Project_2026.Utility/EmailSender.cs
+++ Ecom_Project_2026.Utility/EmailSender.cs
@@ -1,78 +1,59 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
+using brevo_csharp.Api;
+using brevo_csharp.Client;
+using brevo_csharp.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Ecom_Project_2026.Utility
 {
     public class EmailSender : IEmailSender
     {
-        private EmailSettings _emailSettings { get; }
+        private readonly EmailSettings _emailSettings;
+
         public EmailSender(IOptions<EmailSettings> emailSettings)
         {
             _emailSettings = emailSettings.Value;
         }
-        public async Task Execute(string email, string subject, string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"DEBUG >> Domain:[{_emailSettings.PrimaryDomain}] Port:[{_emailSettings.PrimaryPort}] User:[{_emailSettings.UsernameEmail}] PassLength:[{_emailSettings.UsernamePassword?.Length}]");
 
+        public async System.Threading.Tasks.Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
             try
             {
                 string toEmail = string.IsNullOrEmpty(email) ? _emailSettings.FromEmail : email;
-                MailMessage mailMessage = new MailMessage()
+
+                brevo_csharp.Client.Configuration.Default.ApiKey.Add("api-key", _emailSettings.BrevoApiKey);
+
+                var apiInstance = new TransactionalEmailsApi();
+
+                var sender = new SendSmtpEmailSender("Shopping App", _emailSettings.FromEmail);
+
+                var toList = new List<SendSmtpEmailTo>
                 {
-                    From = new MailAddress(_emailSettings.UsernameEmail, "My Email Name")
+                    new SendSmtpEmailTo(toEmail)
                 };
-                mailMessage.To.Add(toEmail);
+
                 if (!string.IsNullOrEmpty(_emailSettings.CcEmail))
                 {
-                    mailMessage.CC.Add(_emailSettings.CcEmail);
+                    // Brevo mein CC ke liye "cc" parameter alag se pass hota hai
                 }
-                //mailMessage.CC.Add(_emailSettings.CcEmail);
-                mailMessage.Subject = "Shopping App : " + subject;
-                mailMessage.Body = message;
-                mailMessage.IsBodyHtml = true;
-                mailMessage.Priority = MailPriority.High;
 
-                //    using (SmtpClient smtpClient = new SmtpClient(_emailSettings.PrimaryDomain,
-                //        _emailSettings.PrimaryPort))
-                //    {
-                //        smtpClient.UseDefaultCredentials = false;   // 👈 Yaha add kiya
-                //        smtpClient.Credentials = new NetworkCredential
-                //            (_emailSettings.UsernameEmail, _emailSettings.UsernamePassword);
-                //        smtpClient.EnableSsl = true;
-                //        await smtpClient.SendMailAsync(mailMessage);
-                //    }
-                //}
-                using (SmtpClient smtpClient = new SmtpClient(
-        _emailSettings.PrimaryDomain,
-        Convert.ToInt32(_emailSettings.PrimaryPort)))
-                {
-                    smtpClient.UseDefaultCredentials = false;
-                    smtpClient.Credentials = new NetworkCredential(
-                        _emailSettings.UsernameEmail,
-                        _emailSettings.UsernamePassword
-                    );
-                    smtpClient.EnableSsl = true;
+                var sendSmtpEmail = new SendSmtpEmail(
+                    sender: sender,
+                    to: toList,
+                    subject: "Shopping App : " + subject,
+                    htmlContent: htmlMessage
+                );
 
-                    await smtpClient.SendMailAsync(mailMessage);
-                }
+                await apiInstance.SendTransacEmailAsync(sendSmtpEmail);
             }
             catch (Exception ex)
             {
                 throw;
             }
         }
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            await Execute(email, subject, htmlMessage);  // ✅ Proper async
-        }
-
     }
-
 }
--- Ecom_Project_2026.Utility/EmailSettings.cs
+++ Ecom_Project_2026.Utility/EmailSettings.cs
@@ -17,5 +17,7 @@
             public string FromEmail { get; set; }
             public string ToEmail { get; set; }
             public string CcEmail { get; set; }
+            public string ResendApiKey { get; set; }
+            public string BrevoApiKey { get; set; }
         }
     }
